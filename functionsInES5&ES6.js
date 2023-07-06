b1752165d8773d4cf99933298b1edb8fe1df1733
Nikhil Prajapati <112732049+Nikhil127001@users.noreply.github.com>
2023-07-07 00:23:36 +0530

    advance javaScript

---
+++ functionsInES5&ES6.js
@@ -0,0 +1,28 @@
+// //Template strings
+// const a = "hellow";
+// const b = "nikhil";
+// let age = 25;
+
+
+// const string = `${a} my name is ${b} and my age is ${age}`;
+// console.log(string);
+
+// // default arguments
+
+// function greet(a='Hiiiii', b = '' , age = 20 ){
+// const string = `${a} my name is ${b} and my age is ${age}`;
+// console.log(string);
+// }
+
+// greet();
+// greet("hellow", "nike", 25);
+
+// arrow functions
+
+//without arrow function 
+function add(a,b){
+    return a+b;
+}
+
+// with arrow funtion
+const add2 = (a,b) => a+b;
